b3370e272799486b28e3f5c0ff9e59569fc3a67f
Nik <njunger@uwaterloo.ca>
2016-06-22 14:48:36 -0400

    BUGFIX: failed to detect client nodes when --client-node omitted

--- src/netmirage-core/graphml.c
+++ src/netmirage-core/graphml.c
@@ -399,7 +399,7 @@ static void graphEndElement(void* ctx, const xmlChar* name) {
 
 		switch (state->dataMode) {
 		case GpNode:
-			if (state->nodeAttribs.typeId && xmlStrEqual(state->dataKey.data, state->nodeAttribs.typeId)) {
+			if (state->clientType != NULL && state->nodeAttribs.typeId && xmlStrEqual(state->dataKey.data, state->nodeAttribs.typeId)) {
 				state->node.t.client = (xmlStrEqual(value, (const xmlChar*)"client"));
 			} else if (state->nodeAttribs.packetLossId && xmlStrEqual(state->dataKey.data, state->nodeAttribs.packetLossId)) {
 				state->node.t.packetLoss = strtod((const char*)value, NULL);
